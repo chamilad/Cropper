80e4e75f52d2b56b235a66b4bf045f4fb6e0c608
Chamila <cs@chamiladealwis.com>
2010-12-02 21:14:39 +0000

    service call returns a "SoapFault - faultcode: 'soapenv:Server' faultstring: 'org.apache.axis2.databinding.ADBException: Unexpected subelement code' faultactor: 'null' detail: org.kxml2.kdom.Node@43e93798" error.

--- src/lk/ucsc/cropper/main.java
+++ src/lk/ucsc/cropper/main.java
@@ -29,34 +29,43 @@ public class main extends Activity {
 	}
 
 	public void onButtonClick(View v) {
-		String NAMESPACE = "http://schemas.icta.lk/xsd/crop/handler/v1/";
+		String NAMESPACE = "http://schemas.icta.lk/xsd/crop/handler/v1";
 		//String NAMESPACE = "http://wtp";
-		String URL = "http://www.srilanka.lk:9080/services/CropServiceProxy.CropServiceProxyHttpSoap12Endpoint";
+		String URL = "http://220.247.225.202:9080/services/CropServiceProxy.CropServiceProxyHttpSoap12Endpoint";
 		//String URL = "http://www.srilanka.lk:9080/services/TemperatureConverterProxy.TemperatureConverterProxyHttpSoap11Endpoint";
 		//String URL = "https://www.srilanka.lk:9443/services/CropServiceProxy.CropServiceProxyHttpsSoap12Endpoint";
 		//String URL = "http://www.srilanka.lk:9080/services/CropServiceProxy.CropServiceProxyHttpSoap11Endpoint";
-		String SOAP_ACTION = "http://schemas.icta.lk/xsd/crop/handler/v1/getCropDataList";
+		//String SOAP_ACTION = "http://schemas.icta.lk/xsd/crop/handler/v1/getCropDataList";
 
 		String method_name = "getCropDataList";
 		//String method_name = "celsiusToFarenheit";
+		String SOAP_ACTION = method_name;
 
 		SoapObject request = new SoapObject(NAMESPACE, method_name);
-		request.addProperty("code", String.valueOf(crop_code.getText().toString()));
+		String crop_code_str = String.valueOf(crop_code.getText().toString());
+		System.out.println(crop_code_str);
+		System.out.println(request.toString());
+		request.addProperty("code", crop_code_str );
+		System.out.println(request.getProperty("code").toString());
 		//request.addProperty("celsius", String.valueOf(crop_code.getText()	.toString()));
 
 		SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
 		envelope.setOutputSoapObject(request);
+		
+		System.out.println("body out : " + envelope.bodyOut.toString());
 
-		AndroidHttpTransport http_transport = new AndroidHttpTransport(URL);
+		//AndroidHttpTransport http_transport = new AndroidHttpTransport(URL);
+		HttpTransportSE http_transport  = new HttpTransportSE(URL);
 		try {
 			http_transport.call(SOAP_ACTION, envelope);
 
-			SoapObject result = (SoapObject) envelope.bodyIn;
+			//SoapObject result = (SoapObject) envelope.bodyIn;
 			//Object result = (Object) envelope.bodyIn;
 			//SoapObject result = (SoapObject) envelope.getResponse();
 			//System.out.println(String.valueOf(result.getProperty("return")));
-			System.out.println(String.valueOf(result.getPropertyCount()));
+			//System.out.println(String.valueOf(result.getPropertyCount()));
 			//System.out.println(result.toString());
+			System.out.println(envelope.bodyIn.toString());
 
 		} catch (Exception e) {
 			e.printStackTrace();
